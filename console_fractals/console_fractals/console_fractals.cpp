f6f66005f54b45af6b71b47cc666159a076e266c
Mihahanya <bs.mihon@gmail.com>
2022-08-11 17:41:56 +0300

    ...

--- console_fractals/console_fractals/console_fractals.cpp
+++ console_fractals/console_fractals/console_fractals.cpp
@@ -2,40 +2,13 @@
 #include <string>
 #include <windows.h>
 
-using std::cout;
-using std::string;
-
 #define KEY_STATE 0x8000
 #define KeyPress(x) GetKeyState(x) & KEY_STATE
 
-#define RST "\x1B[0m"
-#define CBLK "\x1B[30m"
-#define CRED "\x1B[31m"
-#define CGRN "\x1B[32m"
-#define CYEL "\x1B[33m"
-#define CBLU "\x1B[34m"
-#define CMGN "\x1B[35m"
-#define CKYN "\x1B[36m"
-#define CWHT "\x1B[37m"
-#define BCBLK "\x1B[90m"
-#define BCRED "\x1B[91m"
-#define BCGRN "\x1B[92m"
-#define BCYEL "\x1B[93m"
-#define BCBLU "\x1B[94m"
-#define BCMGN "\x1B[95m"
-#define BCKYN "\x1B[96m"
-#define BCWHT "\x1B[97m"
-
-
 namespace csl 
 {
-    const string cols[] = { BCBLK, CBLU, CMGN, CRED, CYEL, CGRN, CKYN, BCBLU, BCMGN, BCRED, BCYEL, BCGRN, BCKYN, CWHT };
-    const int cols_n = 14;
+    const WORD cols[] = {8, 1, 2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14, 7, 15};
     const HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    inline string col_str(const string& s, int c) {
-        return cols[c] + s + RST;
-    }
 
     void gotoxy(int x, int y) {
         COORD coords = {x, y};
@@ -68,34 +41,39 @@ int julia(double x, double y, double cx, double cy, int k) {
 
 int main() 
 {
-    constexpr int size_w = 128/1.5, size_h = 72/1.5;
-    constexpr double size_aspect = double(size_w) / size_h;
+    constexpr int size_w = 128/1.5, size_h = 72/1.5, out_len = size_w*size_h*2;
+    constexpr double size_aspect = (double)size_w / size_h;
 
     csl::set_window(size_w*2, size_h+2, L"Console fractals");
-    system("");
 
-    //const string gradient = " .,:oO0$";
-    //const string gradient = " .:!/r(l1Z4H9W8$@";
-    const string gradient = " `:,!*zTY]kGm%NQ@";
-    //const string gradient = " `.,*+xoOX#0@";
+    //const char gradient[] = " .,:oO0$";
+    //const char gradient[] = " .:!/r(l1Z4H9W8$@";
+    const char gradient[] = " `:,!*zTY]kGm%NQ@";
+    //const char gradient[] = " `.,*+xoOX#0@";
+    const size_t gr_len = strlen(gradient);
 
-    const double move_speed = 0.03, c_speed = 0.005;
-    double nx=0, ny=0, map=1, cx=0, cy=0;
+    const double move_spd = 1e-3, c_spd = 0.5e-3, scale_spd = 1e-3;
+    double nx=0, ny=0, scale=1, cx=0, cy=0;
     int iter_n=10, mode=0;
 
-
     auto timer = clock();
+    double delta_time = 1./60;
     std::ios_base::sync_with_stdio(false);
+
+    CHAR_INFO* frame = new CHAR_INFO[out_len];
+    COORD buff_size {size_w*2, size_h}, buff_coord {0, 0};
+    SMALL_RECT write_region { 0, 0, size_w*2-1, size_h-1 };
+
 
     while (1) 
     {
-        if (KeyPress('W')) ny -= move_speed * map;
-        if (KeyPress('S')) ny += move_speed * map;
-        if (KeyPress('A')) nx -= move_speed * map / size_aspect;
-        if (KeyPress('D')) nx += move_speed * map / size_aspect;
+        if (KeyPress('W')) ny -= move_spd * scale * delta_time;
+        if (KeyPress('S')) ny += move_spd * scale * delta_time;
+        if (KeyPress('A')) nx -= move_spd * scale / size_aspect * delta_time;
+        if (KeyPress('D')) nx += move_spd * scale / size_aspect * delta_time;
 
-        if (KeyPress('Q')) map *= 1.05;
-        if (KeyPress('E')) map *= 0.95;
+        if (KeyPress('Q')) scale *= 1+(scale_spd*delta_time);
+        if (KeyPress('E')) scale *= 1-(scale_spd*delta_time);
 
         if (KeyPress('X')) iter_n += 1;
         if (KeyPress('Z') && iter_n > 2) iter_n -= 1;
@@ -103,39 +81,40 @@ int main()
         if (KeyPress('1')) mode = 0;
         if (KeyPress('2')) mode = 1;
 
-        if (KeyPress('I')) cy -= c_speed * map;
-        if (KeyPress('K')) cy += c_speed * map;
-        if (KeyPress('J')) cx -= c_speed * map / size_aspect;
-        if (KeyPress('L')) cx += c_speed * map / size_aspect;
+        if (KeyPress('I')) cy -= c_spd * scale * delta_time;
+        if (KeyPress('K')) cy += c_spd * scale * delta_time;
+        if (KeyPress('J')) cx -= c_spd * scale / size_aspect * delta_time;
+        if (KeyPress('L')) cx += c_spd * scale / size_aspect * delta_time;
 
 
         int n;
-        string frame{""};
         for (int i = 0; i < size_h; i++) {
             for (int j = 0; j < size_w*2; j++) {
-                double x = double(j - size_w) / size_w * map + nx;
-                double y = double(i*2 - size_h) / size_h * map + ny;
+                double x = (double)(j - size_w) / size_w * scale + nx;
+                double y = (double)(i * 2 - size_h) / size_h * scale + ny;
                 x *= size_aspect;
 
                 if (mode == 0) n = julia(x, y, x, y, iter_n); // Mandelbrot
                 if (mode == 1) n = julia(x, y, cx, cy, iter_n); // Julia
 
-                int grdi = (double)n/iter_n*(gradient.length()-1);
-                int coli = (double)n/iter_n*csl::cols_n;
+                int grdi = (double)n/iter_n*(gr_len-1);
+                int coli = (double)n/iter_n*(sizeof(csl::cols)/sizeof(WORD)-1);
                 
-                frame += csl::col_str(string{gradient[grdi]}, coli);
+                frame[i*size_w*2+j].Char.UnicodeChar = gradient[grdi];
+                frame[i*size_w*2+j].Attributes = csl::cols[coli];
             }
-            frame += '\n';
         }
 
-        double dt = clock()-timer;
+        delta_time = clock()-timer;
         timer = clock();
 
-        csl::gotoxy(0, 0);
-        cout << frame;
-        printf("fps: %.1f, map: x%f, number of iterations: %i, cx: %f, cy: %f ", 
-            1000/dt, 1/map, iter_n, cx, cy);
-        printf("\nWASD - move, QE - zoom, XZ - change number of iterations, 1..2 - mods, IJKL - change component");
+        WriteConsoleOutput(csl::handle, frame, buff_size, buff_coord, &write_region);
+
+        csl::gotoxy(0, size_h-1);
+        printf("fps: %.1f, scale: x%f, number of iterations: %i, cx: %f, cy: %f \n",
+            1000/delta_time, 1/scale, iter_n, cx, cy);
+        printf("WASD - move, QE - zoom, XZ - change number of iterations, 1..2 - mods, IJKL - change component");
     }
+
     return 0;
 }
